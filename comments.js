bf1826e4ef3199550b60af74816ec0367c5400f5
Redouane590 <81474122+Redouane590@users.noreply.github.com>
2023-12-07 20:14:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,60 @@
 // create web server
-const http = require('http');
+// 1. create server
+// 2. register request listener
+// 3. start server
 
-// create web server
-http.createServer((req, res) => {
-    // set response HTTP header with HTTP status and Content type
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    // send the response body "Hello World"
-    res.end('Hello World\n');
-}).listen(8081);
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
 
-// print URL for accessing server
-console.log('Server running at http://:8081/');
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    console.log('middleware 1');
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log('middleware 2');
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log('middleware 3');
+    next();
+})
+
+app.get('/comments', (req, res) => {
+    fs.readFile('./comments.json', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal server error');
+        }
+        res.send(JSON.parse(data));
+    })
+})
+
+app.post('/comments', (req, res) => {
+    fs.readFile('./comments.json', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal server error');
+        }
+        const comments = JSON.parse(data);
+        comments.push(req.body);
+        fs.writeFile('./comments.json', JSON.stringify(comments, null, 2), (err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Internal server error');
+            }
+            res.send('add comment successfully');
+        })
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is running at port 3000');
+})
